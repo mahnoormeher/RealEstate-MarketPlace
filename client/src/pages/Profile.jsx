48f2068130a2228342818cb73d2dd288723c069d
Mahnoor Meher <mahnoorneher@gmail.com>
2025-07-07 15:21:40 +0500

    complete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ export default function Profile() {
 const [uploadSuccess, setUploadSuccess] = useState(false);
 const [updateSuccess , setUpdateSuccess] = useState(false);
 const [formData, setFormData] = useState({});
+const [showListingsError,setShowListingsError] = useState(false);
+const [userListings , setUserListings]  = useState([]);
+
 //console.log(formData)
 
 
@@ -131,6 +134,24 @@ try {
 }
 }
 
+const handleShowListings = async()=>{
+  try {
+    setShowListingsError(false);
+    const res = await fetch (`/api/user/listings/${currentUser._id}`,{
+       credentials: 'include',
+    }
+      
+    );
+const data = await res.json();
+if(data.success ===false){
+  showListingsError(true);
+  return;
+}
+setUserListings(data);
+  } catch (error) {
+    setShowListingsError(true);
+  }
+}
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -190,6 +211,28 @@ try {
       </div>
       <p className='text-red-700 mt-5'>{error? error:''}</p>
       <p className='text-green-600 font-bold mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
+    <button onClick={handleShowListings} className='text-purple-800 w-full text-xl font-semibold'>Show Listings</button>
+    <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
+   
+   {userListings && userListings.length>0 &&
+   <div className='flex flex-col gap-4'>
+    <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+  {userListings.map((listing)=>(
+    <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+       <Link to={`/listing/${listing._id}`}>
+       <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain rounded-lg' />
+       </Link>
+       <Link className='text-blue-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+       <p>{listing.name}</p>
+       </Link>
+       <div className='flex flex-col items-center'>
+          <button className='text-red-700 uppercase'>Delete</button>
+          <button className='text-green-600 uppercase'>Edit</button>
+       </div>
+    </div>
+))
+}
+    </div>}
     </div>
   );
 }
