c087a861bb7b2e96ee19ae73c707ea1141ce79f7
Mahnoor Meher <mahnoorneher@gmail.com>
2025-07-07 17:27:44 +0500

    complete  delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -153,6 +153,25 @@ setUserListings(data);
   }
 }
 
+const handleListingDelete = async(listingId) => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`,{
+      method : 'DELETE',
+    })
+    const data = await res.json();
+    if(data.success === false){
+      console.log(error.message);
+      return;
+    }
+
+    setUserListings((prev)=> 
+      prev.filter((listing)=> listing._id !== listingId)
+  );
+  } catch (error) { 
+    console.log(error.message)
+  }
+}
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7 text-blue-800'>Profile</h1>
@@ -226,7 +245,7 @@ setUserListings(data);
        <p>{listing.name}</p>
        </Link>
        <div className='flex flex-col items-center'>
-          <button className='text-red-700 uppercase'>Delete</button>
+          <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
           <button className='text-green-600 uppercase'>Edit</button>
        </div>
     </div>
